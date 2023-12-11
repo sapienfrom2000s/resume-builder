7f513b816aa6d2fc289cd879e3266d6ffaf19a8f
brickwall <queenisuseless@gmail.com>
2023-12-11 10:42:04 +0530

    Add toggle feature
    - user can toggle between checking a box and entering an end date

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react';
 
 export default function Education({add, handleChange}){
+
+  function toggleEndDate(){
+      let elementToToggle = document.getElementById("endDateBox"); 
+
+    if (elementToToggle.style.display === "none") {
+      elementToToggle.style.display = "block"; 
+    } else {
+      elementToToggle.style.display = "none";
+    }
+  }
+
   return(
     <>
       <label for="name">Institute</label>
       <input type="text" id="name" name="institute" value={ add.institute.name } onChange={(event) => handleChange({name: event.target.value, startDate: add.institute.startDate, endDate: add.institute.endDate}, add.id)} required />
       <label for="startDate">Start Date</label>
       <input type="date" id="startDate" name="startDate" value={ add.institute.startDate } onChange={(event) => handleChange({name: add.name, startDate: event.target.value, endDate: add.institute.endDate}, add.id)} required />
+    <div id='endDateBox'>
       <label for="endDate">End Date</label>
       <input type="date" id="endDate" name="endDate" value={ add.institute.endDate } onChange={(event) => handleChange({name: add.name, endDate: event.target.value, startDate: add.institute.endDate}, add.id)} required />
-    <input type="checkbox" name="currentInstitute" id="currentInstitute" value="value" />
+    </div>
+    <input type="checkbox" onChange={ toggleEndDate }   name="currentInstitute" id="currentInstitute" value="value" />
     <label for="currentInstitute">I currently study here</label>
-
     </>
   )
 }
